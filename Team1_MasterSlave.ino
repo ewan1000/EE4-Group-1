bb92b733d14cb90cce519c2f183cf1e062f7dea3
robertjack25 <114675587+robertjack25@users.noreply.github.com>
2023-01-27 15:34:10 +0000

    Update Team1_MasterSlave.ino

--- Team1_MasterSlave.ino
+++ Team1_MasterSlave.ino
@@ -11,8 +11,7 @@
 byte Z_Offset = 10;
 byte MT1 = 0x03;  //Master transfer 1
 byte MT2 = 0xAA;
-int SLAVE_INIT;
-int digitalSLAVE=0;
+int MasterInit=1;
 int switch_pin = 6;
 int switch_val;
 
@@ -24,12 +23,12 @@ void setup() {
   gpio_pull_up(4);
   gpio_pull_up(5);
   switch_val = digitalRead(switch_pin);
-  if ((switch_val==HIGH) && (digitalSLAVE==0)){     //MASTER SETUP
+  if ((switch_val==HIGH) && (MasterInit==1)){     //MASTER SETUP
     Wire.begin(); 
     Serial.begin(9600); 
     digitalWrite(LED_BUILTIN, HIGH);  //Board LED goes high
   }   // start serial for output
-  else if ((switch_val==LOW) && (digitalSLAVE==1)) {     //SLAVE SETUP    //Potential issue: Does the setup function only get called once??
+  else if ((switch_val==LOW) && (MasterInit==0)) {     //SLAVE SETUP    //Potential issue: Does the setup function only get called once??
     Wire.begin(TEAM1);                                    //Could look into serial.available for acknowledgment, but this should be inside loop function
     Serial.begin(9600);
     Wire.onRequest(requestEvent);
@@ -46,10 +45,10 @@ void requestEvent(int howmany){
 void loop() {
   switch_val = digitalRead(switch_pin);
   //Main block of TEAM1 Master Code
-  if ((switch_val==HIGH) && (digitalSLAVE==0)){              //code to execute when master
+  if ((switch_val==HIGH) && (MasterInit==1)){              //code to execute when master
     delay(1000);
     Wire.beginTransmission(Z_MOTION);  //This is Z-controller info
-    Wire.write(x);        
+    Wire.write(Z_Offset);        
     Wire.endTransmission(); 
     delay(500);
     
@@ -58,14 +57,22 @@ void loop() {
     delay(1000);
     Wire.beginTransmission(TEAM2);  //This is sending handover to team2, Master1 writing to slave2
     Wire.write(MT1);        
-    Wire.write(MT2);     
+   // Wire.write(MT2);     
     Wire.endTransmission(); 
-    digitalSLAVE = 1;
+    MasterInit = 0;
     delay(500);
     setup();
   }
-  else if ((switch_val==LOW) && (digitalSLAVE==1)){
+  else if ((switch_val==LOW) && (MasterInit==0)){
     delay(100);        //slave stuff
     setup();   //this is for changing digital slave back to 0, once team4 is finished
+  }
+  else{
+    while(1){  
+      digitalWrite(LED_BUILTIN, HIGH);  //Board LED goes high
+      delay(500);
+      digitalWrite(LED_BUILTIN, LOW);  //Board LED goes low  
+      delay(500); 
+    }  
   }
 }
