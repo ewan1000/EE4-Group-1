e68b8f00d24a210beb5837515e2a07c2c9f0696a
robertjack25 <114675587+robertjack25@users.noreply.github.com>
2023-02-01 10:23:55 +0000

    Update Team1_MasterSlave_02.ino

--- Team1_MasterSlave_02.ino
+++ Team1_MasterSlave_02.ino
@@ -18,7 +18,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(R_LED, OUTPUT);
   pinMode(G_LED, OUTPUT);
-  pinMode(B_LED, OUTPUT);
+  pinMode(Y_LED, OUTPUT);
   pinMode(switch_pin,INPUT);
   gpio_set_function(4, GPIO_FUNC_I2C);
   gpio_set_function(5, GPIO_FUNC_I2C);
@@ -61,19 +61,19 @@ void receiveEvent(int howmany){
  // int MasterSend2 = Wire.read();   //remember to use this to match up to protocol
   if (MasterSend1 == 3 ){                                               
     digitalWrite(G_LED, HIGH);  //Board LED goes HIGH
-    digitalWrite(B_LED, HIGH);  //Board LED goes LOW
+    digitalWrite(Y_LED, HIGH);  //Board LED goes LOW
     digitalWrite(R_LED, HIGH);  //Board LED goes LOW
     delay(100);        //slave stuff
     digitalWrite(G_LED, LOW);  //Board LED goes HIGH
-    digitalWrite(B_LED, LOW);  //Board LED goes LOW
+    digitalWrite(Y_LED, LOW);  //Board LED goes LOW
     digitalWrite(R_LED, LOW);  //Board LED goes LOW
     delay(100);        //slave stuff
     digitalWrite(G_LED, HIGH);  //Board LED goes HIGH
-    digitalWrite(B_LED, HIGH);  //Board LED goes LOW
+    digitalWrite(Y_LED, HIGH);  //Board LED goes LOW
     digitalWrite(R_LED, HIGH);  //Board LED goes LOW
     delay(100);        //slave stuff
     digitalWrite(G_LED, LOW);  //Board LED goes HIGH
-    digitalWrite(B_LED, LOW);  //Board LED goes LOW
+    digitalWrite(Y_LED, LOW);  //Board LED goes LOW
     digitalWrite(R_LED, LOW);  //Board LED goes LOW
   }
   else {
@@ -85,7 +85,7 @@ void loop() {
   //Main block of TEAM1 Master Code
   if ((digitalRead(switch_pin)==HIGH) && (MasterInit==1)){              //code to execute when master
     digitalWrite(G_LED, HIGH);  //Board LED goes HIGH
-    digitalWrite(B_LED, LOW);  //Board LED goes LOW
+    digitalWrite(Y_LED, LOW);  //Board LED goes LOW
     digitalWrite(R_LED, LOW);  //Board LED goes LOW
     //Master handover to TEAM2
     delay(1000);
@@ -98,14 +98,14 @@ void loop() {
   }
   else if ((digitalRead(switch_pin)==LOW) && (MasterInit==0)){
     digitalWrite(G_LED, LOW);  //Board LED goes LOW
-    digitalWrite(B_LED, HIGH);  //Board LED goes HIGH
+    digitalWrite(Y_LED, HIGH);  //Board LED goes HIGH
     digitalWrite(R_LED, LOW);  //Board LED goes LOW
     delay(100);        //slave stuff
     setup();   //this is for changing digital slave back to 0, once team4 is finished
   }
   else{ 
     digitalWrite(G_LED, LOW);  //Board LED goes LOW
-    digitalWrite(B_LED, LOW);  //Board LED goes LOW
+    digitalWrite(Y_LED, LOW);  //Board LED goes LOW
     digitalWrite(R_LED, HIGH);  //Board LED goes high
     delay(100); 
     setup();                                  
