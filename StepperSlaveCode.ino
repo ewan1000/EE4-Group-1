eeb18ad29953eb455f452a9cd03201326d1e5c94
ewan1000 <115091618+ewan1000@users.noreply.github.com>
2023-02-03 16:02:33 +0000

    Update StepperSlaveCode.ino

--- StepperSlaveCode.ino
+++ StepperSlaveCode.ino
@@ -4,19 +4,26 @@
 
 #include "hardware/i2c.h"
 #include <AccelStepper.h>
-#include <Wire.h>
+#include <Wire.h> 
 
 #define motorInterfaceType 1
+
 // define pin number allocation
 #define dirPin 14
 #define stepPin 15
 #define stepMs1Pin 10
 #define stepMs2Pin 11
 #define stepMs3Pin 12
+const int stepsPerRevolution = 3200;
+
 
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup() {
+  stepper.setMaxSpeed(1000);
+  digitalWrite(stepMs1Pin,HIGH);     //microstepping pins: 16th step
+  digitalWrite(stepMs2Pin,HIGH);
+  digitalWrite(stepMs3Pin,HIGH);
   pinMode(LED_BUILTIN, OUTPUT);
   gpio_set_function(4, GPIO_FUNC_I2C);
   gpio_set_function(5, GPIO_FUNC_I2C);
@@ -29,27 +36,41 @@ void setup() {
 }
 
 void loop() {
-  delay(500);
+   // Set the current position to 0:
+  stepper.setCurrentPosition(0);
+ 
+  // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
+  while(stepper.currentPosition() != 400)
+  {
+    stepper.setSpeed(200);
+    stepper.runSpeed();
+  }
+ 
+  delay(1000);
+ 
+  // Reset the position to 0:
+  stepper.setCurrentPosition(0);
+
 }
 
 void receiveEvent(int howMany) {   //not sure what int howMany is for
   int Data = Wire.read();          // receive byte as an integer
   Serial.println(Data);            // print the integer
   digitalWrite(LED_BUILTIN, HIGH);  //Board LED goes high
-  digitalWrite(dirPin, HIGH);
-  for(int x = 0; x < Data; x++)  {
-    digitalWrite(stepPin,HIGH);        //Forward
-    digitalWrite(stepMs1Pin,HIGH);     //microstepping pins: 16th step
-    digitalWrite(stepMs2Pin,HIGH);
-    digitalWrite(stepMs3Pin,HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin,LOW);         //Reverse
-    digitalWrite(stepMs1Pin,LOW);      //microstepping pins: Full step (200 step/revol) - 1.8 degrees
-    digitalWrite(stepMs2Pin,LOW);
-    digitalWrite(stepMs3Pin,LOW);
-    delayMicroseconds(500);  
+  // Set the current position to 0:
+  stepper.setCurrentPosition(0);
+ 
+  // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
+  while(stepper.currentPosition() != 400)
+  {
+    stepper.setSpeed(200);
+    stepper.runSpeed();
   }
-  delay(1000); //one second delay
+ 
+  delay(1000);
+ 
+  // Reset the position to 0:
+  stepper.setCurrentPosition(0);
   Wire.write("done "); // respond with message of 5 bytes
   // as expected by master   
 }
